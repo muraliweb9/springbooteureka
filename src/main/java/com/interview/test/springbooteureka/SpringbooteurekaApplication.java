032d6087b5caffc277d6a6ea35862fd4ff86fc9f
muraliweb9 <muraliweb@hotmail.com>
2023-04-13 22:11:25 +0100

    Initial

--- src/main/java/com/interview/test/springbooteureka/SpringbooteurekaApplication.java
+++ src/main/java/com/interview/test/springbooteureka/SpringbooteurekaApplication.java
@@ -2,8 +2,10 @@ package com.interview.test.springbooteureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringbooteurekaApplication {
 
 	public static void main(String[] args) {
