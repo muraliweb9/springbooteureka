52e69891f7100d7de46b7efe4161bab720b2478e
muraliweb9 <muraliweb@hotmail.com>
2023-04-13 15:54:28 +0100

    first commit

---
+++ src/main/java/com/interview/test/springbooteureka/SpringbooteurekaApplication.java
@@ -0,0 +1,13 @@
+package com.interview.test.springbooteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooteurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooteurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/test/springbooteureka/SpringbooteurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.test.springbooteureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
